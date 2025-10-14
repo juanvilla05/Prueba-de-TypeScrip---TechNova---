48d6532106125e84c1fcaa78c191b21ba0f97bf8
juanvilla05 <cfinancieramedellin@gmail.com>
2025-10-14 11:03:37 -0500

    cambio de prueba

--- technova/src/pages/index.tsx
+++ technova/src/pages/index.tsx
@@ -107,7 +107,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Goo to nextjs.org →
         </a>
       </footer>
     </div>
